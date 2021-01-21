afd279ae6136be633e6f549e7a28c0780510cefa
Daesol <dae.jung@mail.utoroonto.ca>
2021-01-20 20:11:27 -0500

    add frontend file

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'images/p1.jpg',
+            price:120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category: 'Shirts',
+            image:'images/p2.jpg',
+            price:95,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 12,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Jacket',
+            category: 'Jacket',
+            image:'images/p3.jpg',
+            price:350,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Coat',
+            category: 'Coat',
+            image:'images/p4.jpg',
+            price:280,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 8,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'OG Red Jordan 1',
+            category: 'Shoes',
+            image:'images/p5.jpg',
+            price:175,
+            brand: 'Jordan',
+            rating: 5.0,
+            numReviews: 21,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Supreme Hoodie',
+            category: 'Hoodie',
+            image:'images/p6.jpg',
+            price:385,
+            brand: 'Supreme',
+            rating: 4.5,
+            numReviews: 17,
+            description: 'High quality product'
+        },
+    ]
+};
+export default data;

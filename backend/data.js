ec6c68c903ca88df0eb47d0c8ffacf742dbbdce3
Daesol Jung <daesol97@gmail.com>
2021-01-22 15:44:10 -0500

    created a simple node.js api that req and display product data. Used start command to load backend/server.js everytime there is a change in backend folder

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price:95,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 12,
+            description: 'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Jacket',
+            category: 'Jacket',
+            image:'/images/p3.jpg',
+            price:350,
+            countInStock: 0,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Coat',
+            category: 'Coat',
+            image:'/images/p4.jpg',
+            price:280,
+            countInStock: 5,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 8,
+            description: 'High quality product'
+        },
+        {
+            _id:'5',
+            name:'OG Red Jordan 1',
+            category: 'Shoes',
+            image:'/images/p5.jpg',
+            price:175,
+            countInStock: 5,
+            brand: 'Jordan',
+            rating: 5.0,
+            numReviews: 21,
+            description: 'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Supreme Hoodie',
+            category: 'Hoodie',
+            image:'/images/p6.jpg',
+            price:385,
+            countInStock: 0,
+            brand: 'Supreme',
+            rating: 4.5,
+            numReviews: 17,
+            description: 'High quality product'
+        },
+    ]
+};
+export default data;
